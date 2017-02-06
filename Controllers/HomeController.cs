37cb49143b68f565a3b34e5645182700b39bd7d6
Clemich38 <clement.letreguilly@gmail.com>
2017-02-06 11:57:33 +0900

    remove unuseful template code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,28 +8,10 @@ namespace RSSReader.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View();
-        }
+        } 
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
     }
 }
