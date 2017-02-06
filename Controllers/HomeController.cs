19262ee47aa673fa82daab0239dcfc0ff87a81b8
Clemich38 <clement.letreguilly@gmail.com>
2017-02-06 12:46:15 +0900

    RSS feed title display

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
+
+using RSSReader.ViewModels;
 
 namespace RSSReader.Controllers
 {
@@ -10,7 +14,28 @@ namespace RSSReader.Controllers
     {
         public async Task<IActionResult> Index()
         {
-            return View();
+            var articles = new List<RSSFeedItem>();
+            var feedUrl = "https://blogs.msdn.microsoft.com/martinkearn/feed/";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(feedUrl);
+                var responseMessage = await client.GetAsync(feedUrl);
+                var responseString = await responseMessage.Content.ReadAsStringAsync();
+
+                //extract feed items
+                XDocument doc = XDocument.Parse(responseString);
+                var feedItems = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+                                select new RSSFeedItem
+                                {
+                                    TitleStr = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                                    LinkStr = item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                    ContentStr = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                    // PublishDate = ParseDate(item.Elements().First(i => i.Name.LocalName == "pubDate").Value),
+                                };
+                articles = feedItems.ToList();
+            }
+
+            return View(articles);
         } 
 
     }
