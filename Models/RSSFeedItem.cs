c06061653af322854823542c640879d543da1ae9
Clemich38 <clement.letreguilly@gmail.com>
2017-02-06 11:57:14 +0900

    add rss item model

---
+++ Models/RSSFeedItem.cs
@@ -0,0 +1,10 @@
+
+namespace RSSReader.ViewModels
+{
+  public class RSSFeedItem
+  {
+    public string LinkStr { get; set; }
+    public string TitleStr { get; set; }
+    public string ContentStr { get; set; }
+  }
+}
