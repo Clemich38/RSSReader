3d91c17401f2810f4b47647e0348757e77295a5e
Clemich38 <clement.letreguilly@gmail.com>
2017-02-07 11:54:04 +0900

    rss feed raw display

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,26 +12,49 @@ namespace RSSReader.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string FeedUrl,
+                                               bool DisplayTitle,
+                                               bool DisplayLink,
+                                               bool DisplayContent)
         {
-            IEnumerable<RSSReader.ViewModels.RSSFeedItem> RSSFeedItems = new List<RSSFeedItem>();
-            var feedUrl = "https://blogs.msdn.microsoft.com/martinkearn/feed/";
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(feedUrl);
-                var getResponse = await client.GetAsync(feedUrl);
-                string getResponseString = await getResponse.Content.ReadAsStringAsync();
+            RSSFeedItemList RSSFeedItems = new RSSFeedItemList();
+            RSSFeedItems.ErrorMsg = "";
+            RSSFeedItems.UrlIsValid = true;
+            RSSFeedItems.DisplayTitle = DisplayTitle;
+            RSSFeedItems.DisplayLink = DisplayLink;
+            RSSFeedItems.DisplayContent = DisplayContent;
+            var feedUrl = FeedUrl;//"https://blogs.msdn.microsoft.com/martinkearn/feed/";
 
-                // Extract infos from XML
-                XDocument doc = XDocument.Parse(getResponseString);
-                RSSFeedItems = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
-                                select new RSSFeedItem
-                                {
-                                    TitleStr = item.Elements().First(i => i.Name.LocalName == "title").Value,
-                                    LinkStr = item.Elements().First(i => i.Name.LocalName == "link").Value,
-                                    ContentStr = item.Elements().First(i => i.Name.LocalName == "description").Value,
-                                    // PublishDate = ParseDate(item.Elements().First(i => i.Name.LocalName == "pubDate").Value),
-                                };
+            if(feedUrl != null)
+            {
+                try{
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(feedUrl);
+                        var getResponse = await client.GetAsync(feedUrl);
+                        string getResponseString = await getResponse.Content.ReadAsStringAsync();
+
+                        // Extract infos from XML
+                        XDocument doc = XDocument.Parse(getResponseString);
+                        RSSFeedItems.ItemList = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+                                        select new RSSFeedItem
+                                        {
+                                            TitleStr = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                                            LinkStr = item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                            ContentStr = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                            // PublishDate = ParseDate(item.Elements().First(i => i.Name.LocalName == "pubDate").Value),
+                                        };
+                    }
+                }
+                catch (Exception e){
+                    RSSFeedItems.ErrorMsg = e.Message;
+                    RSSFeedItems.UrlIsValid = false;
+                }
+            }
+            else
+            {
+                RSSFeedItems.ErrorMsg = "Invalid Url";
+                RSSFeedItems.UrlIsValid = false;
             }
 
             return View(RSSFeedItems);
--- Models/RSSFeedItem.cs
+++ Models/RSSFeedItem.cs
@@ -1,6 +1,18 @@
+using System.Collections.Generic;
 
 namespace RSSReader.ViewModels
 {
+  public class RSSFeedItemList
+  {
+    public IEnumerable<RSSFeedItem> ItemList { get; set; }
+    public bool DisplayTitle { get; set; }
+    public bool DisplayLink { get; set; }
+    public bool DisplayContent { get; set; }
+    public bool DisplayDate { get; set; }
+    public string ErrorMsg { get; set; }
+    public bool UrlIsValid { get; set; }
+  }
+
   public class RSSFeedItem
   {
     public string LinkStr { get; set; }
